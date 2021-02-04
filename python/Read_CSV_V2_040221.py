6e83676ad22f729884b24f74bf882f43259fb6c1
ufojava <ufookoro@gmail.com>
2021-02-04 16:27:50 +0000

    Completed the Append write

--- python/Read_CSV_V2_040221.py
+++ python/Read_CSV_V2_040221.py
@@ -1,5 +1,5 @@
 '''
-Synopsis: Script to read csv file and to write to the csv file
+Synopsis: Script to read csv file and to write to the csv file and finally to append to a a file
 
 Wrtiten By: Ufo Okoro
 
@@ -61,7 +61,65 @@ def Write_CSV_File(in_write_file):
 
 
 #Call writer CSV File
-Write_CSV_File(write_file_name)
+#Write_CSV_File(write_file_name)
+
+
+#Funciton to append to a csv file
+def Append_To_CSV_File(in_file_name,in_person_data):
+
+    print(in_person_data)
+    test_data = "Hello world"
+
+    with open(in_file_name, "a",newline="") as get_file:
+
+        #Define file writer
+        file_writer = csv.writer(get_file,delimiter=",")
+
+
+        #Append to file
+        for row in in_person_data:
+
+            file_writer.writerow(row)
+    
+    #Close file
+    get_file.close()
+
+#Empty list to take in data
+person_data = []
+
+def Take_Per_Details():
+    #List ol personla details
+    detail_list = []
+
+    #Take in user details
+    per_firstname = input("Firstname: ")
+    print()
+    per_lastname = input("Lastname: ")
+    print()
+    per_age = input("Age: ")
+
+    #Capitalize the input
+    per_firstname = per_firstname.capitalize()
+    per_lastname = per_lastname.capitalize()
+
+    print()
+    #print(f"{per_firstname} {per_lastname} {per_age}")
+    detail_list.append(per_firstname)
+    detail_list.append(per_lastname)
+    detail_list.append(per_age)
+    
+    return detail_list
+
+
+
+#Get personal data
+person_data.append(Take_Per_Details())
+
+#Pass list into the write to csv file
+
+Append_To_CSV_File(write_file_name,person_data)
+
+
 
 
 
