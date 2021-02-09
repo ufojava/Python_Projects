8130c3675103f7519bdeed76f5dbe3852182ffd4
ufojava <ufookoro@gmail.com>
2021-02-09 08:27:40 +0000

    Answers - Question Testing

--- python/Exam_Questions.py
+++ python/Exam_Questions.py
@@ -14,11 +14,64 @@ import os
 #Variable
 question_counter = 0
 
-
-
-
-#Test
+#Get questions from the Exam question data
 get_questions = Questions()
+
+def Question_Answer(in_question):
+
+    #Variables
+    question_one = "q_one"
+    question_two = "q_two"
+    question_three = "q_three"
+    question_four = "q_four"
+    question_five = "q_five"
+    question_six = "q_six"
+    question_seven = "q_seven"
+    question_eight = "q_eight"
+    question_nine = "q_nine"
+    question_ten = "q_ten"
+
+    
+
+    def Question_One():
+
+        #Variable
+        
+
+        print('''
+        a. Strings, Integers and Floats can be described as Data Types
+        b. String = Text, Integers = Numbers, Floats = Numbers with floating points
+        c. All can be described as Data Structures
+        d. None of the above
+        ''')
+        #Take player answer
+        question_one_answer = input("Select correct answer: ")
+
+        #Test for input type - must be string
+        while not (question_one_answer == "a" or question_one_answer == "b" or question_one_answer == "c" or question_one_answer == "d" ):
+
+            question_one_answer = input("Invalid selection!!! Make Selection: ")
+        
+        if (question_one_answer == "a"):
+
+            print("Correct answer")
+            os.sys.exit()
+        else:
+
+            print("Incorrect answer")
+            os.sys.exit()
+
+    #Call the correct function for the answer function
+    if (question_one):
+
+        Question_One() 
+
+
+
+'''
+*********** Main Program Starts from here ***********
+
+'''
 
 for q_number in get_questions:
 
@@ -33,7 +86,11 @@ for q_number in get_questions:
     print('''
     
     ''')
+    '''
+    if (q_number)
     annswer = input("Select answer to the question: ")
+    '''
+    Question_Answer("q_one")
 
 
 
--- python/Exam_Questions_Data.py
+++ python/Exam_Questions_Data.py
@@ -11,61 +11,46 @@ def Questions():
     #Exam Questions
 
     q_one = '''
-    If a salesman sold 5,000 cars in the year 2020 and has been informed that his target for 2021 has
-    been increased by 3.75 percent
-    How may cars will the salesman be expected to sell in 2021? 
+    How best can you describe Strings, Integers, Floats and Booleans?
     '''
     q_two = '''
-    A client of yours had invested 3 Million pounds in portfolio which is currently paying £2.25 per share.
-    There is an expectation that the share price will sold at £3.75 when the sctock lists on the London Stock exchange.
-    How much will your client be worth after the London listing?
+    In Python, how do you call a String, Integer, Float and Boolean? 
     '''
 
     q_three = '''
-    The company Hong-Kong office will be shutting down due to the world pandemic.
-    Hong-Kong sales represents 25 percent of total company sales (£45,000,000.00)
-    Due to the closure of the office, how much is the company expected to make in
-    the current year?  
+    In Python, what is a list? 
     '''
 
     q_four = '''
-    One of your companys clients has a portfolio worth £3 Million.
-    You sold to the client 15 Percent of his total stock value.
-    How much did you sell?
+    Create a list of numbers 1 to 20
     '''
 
     q_five = '''
-    Your comapny with a valuation of £45 Million has offered you stock options worth 0.75 Percent.
-    How much is you stock worth?
+    Using a For Loop, print the elements in a list containing months of the year
     '''
 
     q_six = '''
-    A child pair of shoes cost £15.99.
-    The value-add tax is 17.5 pecent.
-    How much will you need to present to the teller to complete the sale?
+    How do you best describe a variable ?
     '''
 
     q_seven = '''
-    Your company total staff sick record in 2020 equaled to 10 staff out of a total of 55.
-    Calculate the percentage staff loss in the year 2020? 
+    Create 3 variable that stores the string, Integer and Float values
     '''
 
     q_eight = '''
-    The price of oil in 2020 stood at $34 per barrel.
-    A shortagte in the current year as triggered a price increase to $42 per barrel.
-    Calculate the percentage price increase.
+    Using the IF, ELIF and ELSE Conditional statements, create a code block that will:
+    1. Check a variable value of 20 and print "This is 20"
+    2. Check another variable value of 30 and print "This is 30"
+    3. Else print "This is neither 20 or 30"
     '''
 
     q_nine = '''
-    Your gross salary is £35,000.00 per anum.
-    The tax is calculated at 22 Percent.
-    You are entiled to £10,000.00 tax allowance for the year.
-    What will be your net salary for the year?
+    Create a function that will return the sum of 2 numbers
     '''
 
     q_ten = '''
-    Bonus Question:
-    Whats is 18 percent of 5 Million barrels of oil
+    Create a Function that will print "This is my first function"
+    Call the function by assinging to a variable
     '''
 
     list_of_questions = [q_one, q_two, q_three, q_four, q_five, q_six, q_seven, q_eight, q_nine, q_ten]
