0d9bc7cd7b1b5e14eee36f7ba9567142e50d71dd
ufojava <ufookoro@gmail.com>
2021-03-27 11:23:06 +0000

    Calss Roll Dice

--- python/Game_School_Run.py
+++ python/Game_School_Run.py
@@ -59,9 +59,44 @@ class Player_Name:
         self.player_lastname = player_lastname
 
 
+#Class to roll dice
+class Roll_Dice:
+
+    def __init__(self,player_firstname,player_lastname):
+
+        self.player_firstname = player_firstname
+        self.player_lastname = player_lastname
+
+    
+    #Roll Dice
+    def Roll(self):
+
+        import random
+
+        #Get player names
+        get_player_firstname = self.player_firstname
+        get_player_lastname = self.player_lastname
+
+        #Possible Dice Numbers
+        dice_numbers = [0,1,3,6]
+
+        #Roll dice
+        player_roll_dice = random.choice(dice_numbers)
+
+        return player_roll_dice
+
+
 #Process player details
 process_player_input = Player_Name(get_player_input[0].capitalize(), get_player_input[1].capitalize())
 
 
-print(process_player_input.player_firstname, process_player_input.player_lastname)
+#print(process_player_input.player_firstname, process_player_input.player_lastname)
+
+process_player_roll_dice = Roll_Dice(process_player_input.player_firstname,process_player_input.player_lastname)
+
+#Get player name and rolled dice
+print()
+print(process_player_roll_dice.player_firstname,process_player_roll_dice.player_lastname,process_player_roll_dice.Roll())
+
+
 
